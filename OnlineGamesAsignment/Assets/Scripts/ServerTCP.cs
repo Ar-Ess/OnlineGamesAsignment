387b33ee2d864fd18cdb9780edf0186c91b9ce9b
Jordi Espriu <37652360+LordUnicorn31@users.noreply.github.com>
2022-10-10 10:31:03 +0200

    threading

--- OnlineGamesAsignment/Assets/Scripts/ServerTCP.cs
+++ OnlineGamesAsignment/Assets/Scripts/ServerTCP.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 public class ServerTCP : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class ServerTCP : MonoBehaviour
     IPEndPoint ipep;
     int port;
     byte[] data;
+    Thread listenThread;
 
     private void Awake()
     {
@@ -18,20 +20,27 @@ public class ServerTCP : MonoBehaviour
         data = new byte[1024];
         newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         ipep = new IPEndPoint(IPAddress.Any, port);
+        listenThread = new Thread(Listen);
     }
     // Start is called before the first frame update
     void Start()
     {
         newSocket.Bind(ipep);
-        newSocket.Listen(10);
         client = newSocket.Accept();
         newSocket.Connect(ipep);
+        listenThread.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
+        listenThread.Start();
         int recv = client.Receive(data);
         client.Send(data, recv, SocketFlags.None);
+    }
+
+    void Listen()
+    {
+        newSocket.Listen(10);
     }
 }
