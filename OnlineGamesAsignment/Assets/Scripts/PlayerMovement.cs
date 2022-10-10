9a712b3885139750ea3ee53fa03cf3d67387b811
BooStarGamer <marti.buxeda@gmail.com>
2022-10-10 10:17:49 +0200

    OGA - Lobby Scene

---
+++ OnlineGamesAsignment/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] Camera camera;
+    [SerializeField] Transform spawnpoint;
+
+    [Header("Physics")]
+    [SerializeField] float speed = 6000;
+    [SerializeField] float acceleration = 1;
+    [SerializeField] float jumpForce = 5000;
+
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera.transform.position = new Vector3(0, 33, -36);
+        camera.transform.rotation = Quaternion.Euler(13, 0, 0);
+        transform.position = spawnpoint.position;
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity = new Vector3(0, -200, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float acc = 1;
+        if (Input.GetKey(KeyCode.LeftShift)) acc = acceleration;
+
+        if (Input.GetKey(KeyCode.A)) rb.AddForce(new Vector3(-speed * Time.deltaTime * acc, 0, 0));
+
+        if (Input.GetKey(KeyCode.D)) rb.AddForce(new Vector3( speed * Time.deltaTime * acc, 0, 0));
+
+        if (Input.GetKeyDown(KeyCode.W)) rb.AddForce(new Vector3(0, jumpForce * Time.deltaTime, 0), ForceMode.Impulse);
+
+    }
+}
