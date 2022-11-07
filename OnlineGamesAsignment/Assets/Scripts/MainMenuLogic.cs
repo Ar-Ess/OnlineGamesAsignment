0250e8f884f920dd9aaf2bd2acc57fa65ad013bf
BooStarGamer <marti.buxeda@gmail.com>
2022-11-07 09:36:57 +0100

    OGA - New remodelation

---
+++ OnlineGamesAsignment/Assets/Scripts/MainMenuLogic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuLogic : MonoBehaviour
+{
+    public void ChangeScene(string scene)
+    {
+        Debug.Log(scene);
+        switch (scene)
+        {
+            case "ClientConnectScene":
+            case "ServerConnectScene":
+                SceneManager.LoadScene(scene);
+                break;
+        }
+    }
+}
