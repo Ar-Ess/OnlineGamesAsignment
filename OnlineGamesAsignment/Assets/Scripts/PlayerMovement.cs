0742e41d10b370f5cff485f3a03da69937f19809
Jordi Espriu <37652360+LordUnicorn31@users.noreply.github.com>
2022-11-12 13:52:35 +0100

    Lots of stuff
    - Movement enhanced
    - Scale reduced to work with small numbers
    - Sprites added

--- OnlineGamesAsignment/Assets/Scripts/PlayerMovement.cs
+++ OnlineGamesAsignment/Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Transform spawnpoint;
 
     [Header("Physics")]
-    [SerializeField] float speed = 6000;
+    [SerializeField] float speed = 50;
     [SerializeField] float acceleration = 1;
-    [SerializeField] float jumpForce = 5000;
+    [SerializeField] float jumpForce = 50;
     [SerializeField] float globalGravity = 10;
     [SerializeField] string state = string.Empty;
     Serializer serializer = new Serializer();
@@ -23,58 +23,40 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody rb;
 
-    void Start()
+    private void Start()
     {
-        camera.transform.position = new Vector3(0, 33, -36);
-        camera.transform.rotation = Quaternion.Euler(13, 0, 0);
-        transform.position = spawnpoint.position;
         rb = GetComponent<Rigidbody>();
-        Physics.gravity = new Vector3(0, -200, 0);
-        rb.useGravity = false;
-        ground = false;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        camera.transform.position = new Vector3(transform.position.x, transform.position.y + 33, -36);
-
-        Vector3 gravity = -globalGravity * gravityScale * Vector3.up;
-        rb.AddForce(gravity, ForceMode.Acceleration);
+        PlayerMove();
+        if (Input.GetKey(KeyCode.Space) && ground) Jump();
     }
 
-    void Update()
+    private void PlayerMove()
     {
-
-        float acc = 1;
-        if (Input.GetKey(KeyCode.LeftShift)) acc = acceleration;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(new Vector3(-speed * Time.deltaTime * acc, 0, 0));
-            state = "Moving right";
-            
-            stream = serializer.Serialize(state);
-            
-        }
-
-        if (Input.GetKey(KeyCode.D)) rb.AddForce(new Vector3(speed * Time.deltaTime * acc, 0, 0));
-
-        if (Input.GetKeyDown(KeyCode.Space)) Jump();
-
-        serializer.Deserialize(recStream);
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
     }
 
-    void OnCollisionEnter(Collision hit)
+    private void Jump() => rb.velocity = new Vector2(0, jumpForce);
+
+    //private bool IsGrounded()
+    //{
+    //    var groundCheck = Physics.Raycast(transform.position, Vector2.down, 0.7f);
+    //    return groundCheck.CompareTo != null && groundCheck.CompareTag("Ground");
+    //}
+
+    private void OnCollisionStay(Collision collision)
     {
-        if (hit.gameObject.CompareTag("Ground"))
-        {
-            ground = true;
-        }
+        if(collision.collider.tag == "Ground")
+        ground = true;
     }
 
-    private void Jump()
+    private void OnCollisionExit(Collision collision)
     {
-        rb.AddForce(Vector2.up * jumpForce, ForceMode.Acceleration);
-        ground = false;
+        if (collision.collider.tag == "Ground")
+            ground = false;
     }
 }
